ea423bb224ae8528223f33f81d461a3e78394da8
naruepol <naruapon@gmail.com>
2022-02-22 10:30:35 +0700

    Initial commit

---
+++ Test Suites/LoginDataDriven.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LoginDataDriven</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>3be46b21-94c6-45b3-a089-65083b639111</testSuiteGuid>
+   <testCaseLink>
+      <guid>b08b805d-0b34-4fc4-9fb8-75a6615b90dd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC101 Login (Sucesss-DataDrivenTesting)</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>413731c3-1823-4cfc-b081-4a83a7e3076a</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/LoginData</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>413731c3-1823-4cfc-b081-4a83a7e3076a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>c2ef76df-354c-4506-bd6d-6c7def4234b7</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>413731c3-1823-4cfc-b081-4a83a7e3076a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>54ece188-8e04-4040-8dba-7c9b739ef760</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
